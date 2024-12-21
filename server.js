1ea7ddc782e96038ef978aa0b59f45237f2443fc
Sneha <snehaarora68791@gmail.com>
2024-12-21 19:08:45 +0530

    ok

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Feedback = require('./mongo'); 
+const upload = require("./multer"); 
 const app = express();
 const PORT = 4020;
 
@@ -19,14 +20,33 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/Feedback.html'); 
 });
 
-app.post('/submit', (req, res) => {
-    const feedbackData = new Feedback(req.body);
+app.post('/submit', upload.fields([{ name: 'maintenancePhotos', maxCount: 5 }, { name: 'amenitiesPhotos', maxCount: 5 }]), (req, res) => {
+    console.log(req.files); 
+
+    const feedbackData = new Feedback({
+        ...req.body,
+        maintenancePhotos: req.files['maintenancePhotos'] ? req.files['maintenancePhotos'].map(file => file.filename) : [],
+        amenitiesPhotos: req.files['amenitiesPhotos'] ? req.files['amenitiesPhotos'].map(file => file.filename) : []
+    });
+
     feedbackData.save()
     .then(() => {
         res.send("Feedback submitted successfully!");
     })
     .catch(err => {
         res.status(400).send('Error saving feedback: ' + err);
+    });
+});
+
+app.get('/feedback', (req, res) => {
+    Feedback.find()
+    .then(feedbacks => {
+        console.log(feedbacks); 
+        res.json(feedbacks); 
+    })
+    .catch(err => {
+        console.error('Error retrieving feedback:', err);
+        res.status(500).send('Error retrieving feedback: ' + err);
     });
 });
 
